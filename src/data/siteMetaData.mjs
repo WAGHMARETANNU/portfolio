2f21409b1e401ce54150988e67716aabbb2f8a96
WAGHMARE TANNU <tannuwaghmare15@gmail.com>
2026-01-09 18:20:51 +0530

    run

--- src/data/siteMetaData.mjs
+++ src/data/siteMetaData.mjs
@@ -25,3 +25,4 @@ export const siteMetadata = {
 // Next.js sometimes prefers default exports for .mjs, 
 // but your components are using Named Imports { siteMetadata }
 export default siteMetadata;
+export { siteMetadata };
