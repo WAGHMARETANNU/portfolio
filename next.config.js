4fe2af2e3dc037e625801518e71399091e7e3cff
WAGHMARE TANNU <tannuwaghmare15@gmail.com>
2026-01-10 22:05:16 +0530

    debugging

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  
+  images: {
+    unoptimized: true, // Add this line
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
